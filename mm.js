2cfed6ca728cff7a66823c48002868c4b81997af
sunnydesale001 <sunnydesale001@gmail.com>
2025-06-21 23:13:01 +0530

    add enitial file

---
+++ mm.js
@@ -0,0 +1,76 @@
+// for(let i=0;i<3,i++)
+// {
+//     for( let j=0; j<3,j++)
+// {
+// }
+//     log('*')
+// }
+
+// var a ="";
+// for( i=1;i<=4; i++){
+
+//     for (j=1;j<=1;j++){
+//         a+="*";
+//     }
+//     console.log(a)
+// }
+
+// let n = 5;
+
+// for (let i = 4; i >= 0; i--) {
+//     let line = '';
+
+    
+//     for (let j = 1; j <= n - i; j++) {
+//         line += ' ';
+//     }
+//     for (let k = 1; k <= 2 * i - 1; k++) {
+//         line += '*';
+//     }
+
+//     console.log(line);
+// }
+
+// for(var i=1;i<=3;i++){
+//   var str=""
+//   for(var j=1;j<=i;j++){
+//     str=str+"*"
+//   }
+//   for(var j=1;j<=6-2*i;j++){
+//     str=str+" "
+//   }
+//   for(var j=1;j<=i;j++){
+//     str=str+"*"
+//   }
+//   console.log(str);
+// }
+
+
+// for(i=3;i>=1;i--){
+//   var str=""
+//   for(var j=1;j<=i;j++){
+//     str=str+"*"
+//   }
+//   for(var j=1;j<=6-2*i;j++){
+//     str=str+" "
+//   }
+//   for(var j=1;j<=i;j++){
+//     str=str+"*"
+//   }
+//   console.log(str);
+  
+// }
+ 
+for( var i=1; i<=3; i++){
+    str=" "
+for( var j=1; j<=i; i++){
+    str=str=+"*"
+}    
+for(var j=1; j<=8; i++) {
+   str=str+" "
+}  
+for(var j=1; j<=i; i++){
+   str=str+"*" 
+}
+console.log(str);
+}
